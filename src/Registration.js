5c46d9e4c64ea0ae4debf67965baa942983ff4c3
Mateusz56 <mateusz.kozyra@titanitegames.com>
2021-05-28 13:02:51 +0200

    add registration form

--- src/Registration.js
+++ src/Registration.js
@@ -1,11 +1,99 @@
 import React, {Component} from 'react';
+import {Form} from "react-bootstrap";
 
 class Registration extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+
+        fetch("http://localhost:8000/user/", {
+            method: "POST",
+            headers: {
+                'content-type': "application/json",
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password,
+                email: this.state.email,
+                first_name: this.state.name,
+                last_name: this.state.surname
+            })
+        }).then((respone) => alert(respone.status))
+
+
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <Form onSubmit={this.handleSubmit.bind(this)}>
+                <Form.Label>
+                    Nazwa użytkownika:
+                    <Form.Control
+                        name="username"
+                        type="text"
+                        onChange={this.handleInputChange} />
+                </Form.Label>
+                <br />
+                <Form.Label>
+                    Hasło:
+                    <Form.Control
+                        name="password"
+                        type="password"
+                        onChange={this.handleInputChange} />
+                </Form.Label>
+                <br />
+                <Form.Label>
+                    Powtórz hasło:
+                    <Form.Control
+                        name="repeatpassword"
+                        type="password"
+                        onChange={this.handleInputChange} />
+                </Form.Label>
+                <br />
+                <Form.Label>
+                    Imię:
+                    <Form.Control
+                        name="name"
+                        type="text"
+                        onChange={this.handleInputChange} />
+                </Form.Label>
+                <br />
+                <Form.Label>
+                    Nazwisko:
+                    <Form.Control
+                        name="surname"
+                        type="text"
+                        onChange={this.handleInputChange} />
+                </Form.Label>
+                <br />
+                <Form.Label>
+                    E-mail:
+                    <Form.Control
+                        name="email"
+                        type="text"
+                        onChange={this.handleInputChange} />
+                </Form.Label>
+                <br />
+                <input type="submit" value="Wyślij" onSubmit={this.handleSubmit.bind(this)}/>
+            </Form>
         );
     }
 }
