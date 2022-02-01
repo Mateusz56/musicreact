5a851f99bd5f033a48faa4ca17cebd5c02715308
Mateusz <mateusz.kozyra56@gmail.com>
2022-02-01 20:03:24 +0100

    Update Error403.js

--- src/Error403.js
+++ src/Error403.js
@@ -5,7 +5,7 @@ const Error403 = () => {
     return (
         <div>
             <h1>{Translations.GetText('error403')}</h1>
-            <h3>{Translations.GetText('noPermissions')}</h3>
+            <h3>{Translations.GetText('noPermission')}</h3>
         </div>
     );
 };
